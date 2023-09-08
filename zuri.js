c3924925e97295afef6b858190e158500de4dba0
Blessedfriday007 <Fridayblessed333@gmail.com>
2023-09-08 17:48:02 +0100

    massive modifications made

--- zuri.js
+++ zuri.js
@@ -1,13 +1,22 @@
-function updateUTCTime() {
-    const utcTimeElement = document.getElementById('utc-time');
-      const currentTimeMillis = new Date().getTime();
-      const currentTimeUTC = new Date(currentTimeMillis).toUTCString();
-      utcTimeElement.textContent = currentTimeUTC;
-  
-      console.log("UTC Time uploaded")
-  }
-  
-  document.getElementById('refresh-btn').addEventListener('click', updateUTCTime);
-  
-  // Initial time update
-  updateUTCTime();
+const weekday=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        const d=new Date();
+        let day= weekday[d.getDay()];
+        document.getElementById('datetoday').innerHTML=day;
+
+
+
+        const now = new Date();
+
+
+const hours = now.getUTCHours();
+const minutes = now.getUTCMinutes();
+const seconds = now.getUTCSeconds();
+const milliseconds = now.getUTCMilliseconds();
+
+
+
+            let time=(`${hours}:${minutes}:${seconds}.${milliseconds}`);
+
+            document.getElementById('timenow').innerHTML=time;
+document.getElementById('refresh-btn').addEventListener('click', updateUTCTime);
+
